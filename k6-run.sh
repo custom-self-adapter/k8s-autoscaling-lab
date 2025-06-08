a08cd2da6ca2d91b0f0a0ac9e6c6515131a3bc05
Humberto Fraga <xisberto@gmail.com>
2025-06-08 11:53:11 -0300

    Uses $1 to get the script name

--- k6-run.sh
+++ k6-run.sh
@@ -5,9 +5,11 @@ then
     ./k6-go-build.sh
 fi
 
-TESTNAME='ob-loadtest'
+DEFAULTTESTNAME='ob-loadtest'
+TESTNAME="${1:-$DEFAULTTESTNAME}"
 export K6_PROMETHEUS_RW_SERVER_URL='http://10.0.0.17:9090/api/v1/write'
+export BASE_URL='http://boutique.k8s.lab'
 
 ./k6 run tests/scenarios/$TESTNAME.js \
-    -o csv=tests/results/$TESTNAME-$(date '+%Y-%m-%d-%H%M').csv \
     -o experimental-prometheus-rw
+    # -o csv=tests/results/$TESTNAME-$(date '+%Y-%m-%d-%H%M').csv \

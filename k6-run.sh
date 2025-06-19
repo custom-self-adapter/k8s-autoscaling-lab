66aafda0ffff305943c07e81337b0d9f23beb453
Humberto Fraga <xisberto@gmail.com>
2025-06-19 08:17:54 -0300

    Switches target from online-boutique to kube-znn

--- k6-run.sh
+++ k6-run.sh
@@ -8,7 +8,7 @@ fi
 DEFAULTTESTNAME='ob-loadtest'
 TESTNAME="${1:-$DEFAULTTESTNAME}"
 export K6_PROMETHEUS_RW_SERVER_URL='http://10.0.0.17:9090/api/v1/write'
-export BASE_URL='http://boutique.k8s.lab'
+export BASE_URL='http://znn.k8s.lab'
 
 ./k6 run tests/scenarios/$TESTNAME.js \
     -o experimental-prometheus-rw
---
+++ tests/scenarios/znn-loadtest.js
@@ -0,0 +1,17 @@
+import http from "k6/http";
+import { check } from "k6";
+
+export const options = {
+    vus: 500,
+    duration: "5m",
+    thresholds: {
+        http_req_duration: ["p(95)<2500"],
+    },
+};
+
+const BASE_URL = __ENV.BASE_URL || "http://localhost:8080";
+
+export default function () {
+    const res = http.get(`${BASE_URL}/news.php`);
+    check(res, { "news OK": (r) => r.status === 200 });
+}

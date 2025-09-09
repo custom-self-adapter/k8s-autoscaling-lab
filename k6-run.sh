accc1c527ba9b6de5bca908242fd24fb85b9d632
Humberto Fraga <humbertofraga@gmail.com>
2025-09-09 07:44:33 -0300

    Better use of parameters on k6 run script

--- k6-run.sh
+++ k6-run.sh
@@ -5,11 +5,19 @@ then
     ./k6-go-build.sh
 fi
 
-DEFAULTTESTNAME='ob-loadtest'
+DEFAULTTESTNAME='znn-loadtest'
 DEFAULTBASEURL='http://znn.k8s.lab'
+DEFAULTPROMURL='http://prometheus.k8s.lab'
 export BASE_URL="${1:-$DEFAULTBASEURL}"
-TESTNAME="${2:-$DEFAULTTESTNAME}"
+PROM_URL="${2:-$DEFAULTPROMURL}"
+TESTNAME="${3:-$DEFAULTTESTNAME}"
+
+export K6_PROMETHEUS_RW_SERVER_URL="${PROM_URL}/api/v1/write"
+export K6_PROMETHEUS_RW_TREND_STATS='min,max,p(95),p(99)'
+
+TIMESTAMP=$(date '+%Y%m%d%H%M')
 
 ./k6 run tests/scenarios/$TESTNAME.js \
-    --tag testid=$(date +%s) \
-    -o csv=tests/results/$TESTNAME-$(date '+%Y-%m-%d-%H%M').csv
+    --tag testid=$TIMESTAMP \
+    -o experimental-prometheus-rw \
+    -o csv=tests/results/$TESTNAME-$TIMESTAMP.csv

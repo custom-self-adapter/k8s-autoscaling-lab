9b92c0ec49378b6bf2ef2f40c2ae8eca736a9b4c
Humberto Fraga <humbertofraga@gmail.com>
2025-09-20 11:38:19 -0300

    Apply labels and nodeSelector to metrics-sever

--- cluster_bootstrap.sh
+++ cluster_bootstrap.sh
@@ -5,8 +5,10 @@ VAGRANT_CERTS='vagrant-kubeadm-kubernetes/certs'
 kubectl label node node01 app=yes
 kubectl label node node02 db=yes
 kubectl label node node02 adaptation=yes
+kubectl label node node02 cert=yes
 kubectl label node node02 ingress=yes
 kubectl label node node03 monitoring=yes
+kubectl patch -n kube-system deployments.apps metrics-server -p '{"spec": {"template": {"spec": {"nodeSelector": {"monitoring": "yes"}}}}}'
 
 helmfile sync
 

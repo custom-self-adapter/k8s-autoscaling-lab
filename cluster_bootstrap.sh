a95e06450e041aff16c6897280b6bc73560d05fd
Humberto Fraga <humbertofraga@gmail.com>
2026-03-07 09:47:22 -0300

    Updates cluster configuration

--- cluster_bootstrap.sh
+++ cluster_bootstrap.sh
@@ -2,13 +2,14 @@
 
 VAGRANT_CERTS='vagrant-kubeadm-kubernetes/certs'
 
-kubectl label node node01 app=yes
-kubectl label node node02 db=yes
-kubectl label node node02 adaptation=yes
-kubectl label node node02 cert=yes
-kubectl label node node03 ingress=yes
-kubectl label node node03 monitoring=yes
-kubectl patch -n kube-system deployments.apps metrics-server -p '{"spec": {"template": {"spec": {"nodeSelector": {"monitoring": "yes"}}}}}'
+kubectl label node node01 autoscaling.lab/app=yes
+kubectl label node node02 autoscaling.lab/db=yes
+kubectl label node node02 autoscaling.lab/adaptation=yes
+kubectl label node node02 autoscaling.lab/cert=yes
+kubectl label node node02 autoscaling.lab/ingress=yes
+kubectl label node node03 autoscaling.lab/registry=yes
+kubectl label node node03 autoscaling.lab/monitoring=yes
+kubectl patch -n kube-system deployments.apps metrics-server -p '{"spec": {"template": {"spec": {"nodeSelector": {"autoscaling.lab/monitoring": "yes"}}}}}'
 
 helmfile sync
 

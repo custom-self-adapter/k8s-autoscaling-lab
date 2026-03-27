f23a2fb7e79754433d1430d7324a27d2d90494aa
Humberto Fraga <humbertofraga@gmail.com>
2026-03-27 19:46:18 -0300

    New cluster configuration

--- cluster_bootstrap.sh
+++ cluster_bootstrap.sh
@@ -18,5 +18,3 @@ kubectl apply -f bootstrapping/ipaddresspool.yaml
 kubectl create namespace cert-manager
 kubectl -n cert-manager create secret tls lab-ca-keypair --cert=$VAGRANT_CERTS/rootCA.crt --key=$VAGRANT_CERTS/rootCA.key
 kubectl apply -f bootstrapping/cluster-certs.yaml
-
-kubectl apply -f bootstrapping/ingress-cert.yaml

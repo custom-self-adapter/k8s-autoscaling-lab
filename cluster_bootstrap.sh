15462e87de64b55c2e8a19af6b987acb8cc4c2a3
Humberto Fraga <humbertofraga@gmail.com>
2026-03-28 12:09:12 -0300

    Separates cluster bootstrap into two steps

--- cluster_bootstrap.sh
+++ cluster_bootstrap.sh
@@ -11,10 +11,12 @@ kubectl label node node03 autoscaling.lab/registry=yes
 kubectl label node node03 autoscaling.lab/monitoring=yes
 kubectl patch -n kube-system deployments.apps metrics-server -p '{"spec": {"template": {"spec": {"nodeSelector": {"autoscaling.lab/monitoring": "yes"}}}}}'
 
-helmfile sync
+helmfile sync -f helmfile_step1.yaml
 
 kubectl apply -f bootstrapping/ipaddresspool.yaml
-
-kubectl create namespace cert-manager
 kubectl -n cert-manager create secret tls lab-ca-keypair --cert=$VAGRANT_CERTS/rootCA.crt --key=$VAGRANT_CERTS/rootCA.key
 kubectl apply -f bootstrapping/cluster-certs.yaml
+kubectl create namespace ingress-nginx
+kubectl apply -f bootstrapping/ingress-cert.yaml
+
+helmfile sync -f helmfile_step2.yaml
